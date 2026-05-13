6576e085d8a6d0f59d063982c62512f6d6a44cf8
nadia vlasenko <nadiia@MacBook-Air-nadia.local>
2026-05-13 20:03:56 +0200

    implement responsive hero section

---
+++ src/components/HeroSection/HeroSection.tsx
@@ -0,0 +1,36 @@
+import "./HeroSection.scss";
+import bgHero from "../../assets/images/bg-pictures/bgHero.png";
+import arrowRight from "../../assets/images/icons/arrow-right.svg";
+
+export const HeroSection = () => {
+  return (
+    <section className="hero-section">
+      <div className="hero-section__content">
+        <h1 className="hero-section__title">Your Personal Sommelier</h1>
+
+        <div className="hero-section__subtitle">
+          <span />
+          <p>Curate your collection</p>
+          <span />
+        </div>
+
+       <button className="hero-section__button" type="button">
+  Explore Collection
+
+  <img
+    src={arrowRight}
+    alt="Arrow right"
+    className="hero-section__button-arrow"
+  />
+</button>
+      </div>
+
+      <div className="hero-section__image">
+  <img
+    src={bgHero}
+    alt="Wine"
+  />
+</div>
+    </section>
+  );
+};
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-
 import "./HomePage.scss";
+import { HeroSection } from "../../components/HeroSection";
 
 export const HomePage: React.FC = () => {
   return (
     <section className="home__page">
       <div className="container">
-        <h1 className="home_page--title">WineMood</h1>
+        <HeroSection/>
       </div>
     </section>
   );

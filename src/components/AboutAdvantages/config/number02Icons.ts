1f92c1dc0384adea161bbe01c383af210697faeb
nadia vlasenko <nadiia@MacBook-Air-nadia.local>
2026-06-02 20:40:28 +0200

    implement About Advantages section with responsive glow cards

---
+++ src/components/AboutAdvantages/config/number02Icons.ts
@@ -0,0 +1,13 @@
+import defaultIcon from "@/assets/images/AboutAdvantages/icons/02/02-default.svg";
+import celebrationIcon from "@/assets/images/AboutAdvantages/icons/02/02-celebration.svg";
+import cozyIcon from "@/assets/images/AboutAdvantages/icons/02/02-cozy-evening.svg";
+import dateNightIcon from "@/assets/images/AboutAdvantages/icons/02/02-date-night.svg";
+import culinaryIcon from "@/assets/images/AboutAdvantages/icons/02/02-culinary.svg";
+
+export const number02Icons = {
+  default: defaultIcon,
+  celebration: celebrationIcon,
+  cozy: cozyIcon,
+  dateNight: dateNightIcon,
+  culinary: culinaryIcon,
+};
---
+++ src/components/AboutAdvantages/config/number03Icons.ts
@@ -0,0 +1,13 @@
+import defaultIcon from "@/assets/images/AboutAdvantages/icons/03/03-default.svg";
+import celebrationIcon from "@/assets/images/AboutAdvantages/icons/03/03-celebration.svg";
+import cozyIcon from "@/assets/images/AboutAdvantages/icons/03/03-cozy-evening.svg";
+import dateNightIcon from "@/assets/images/AboutAdvantages/icons/03/03-date-night.svg";
+import culinaryIcon from "@/assets/images/AboutAdvantages/icons/03/03-culinary.svg";
+
+export const number03Icons = {
+  default: defaultIcon,
+  celebration: celebrationIcon,
+  cozy: cozyIcon,
+  dateNight: dateNightIcon,
+  culinary: culinaryIcon,
+};

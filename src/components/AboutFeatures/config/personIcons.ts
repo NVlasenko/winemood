8430ac0095135b376f4c85c9c3c2e0d0c8dce78e
nadia vlasenko <nadiia@MacBook-Air-nadia.local>
2026-06-02 19:25:15 +0200

    add About Features section with animated cards

---
+++ src/components/AboutFeatures/config/personIcons.ts
@@ -0,0 +1,13 @@
+import defaultIcon from "@/assets/images/aboutUs/icons/person/person-default.svg";
+import celebrationIcon from "@/assets/images/aboutUs/icons/person/person-celebration.svg";
+import cozyIcon from "@/assets/images/aboutUs/icons/person/person-cozy-evening.svg";
+import dateNightIcon from "@/assets/images/aboutUs/icons/person/person-date-night.svg";
+import culinaryIcon from "@/assets/images/aboutUs/icons/person/person-culinary.svg";
+
+export const personIcons = {
+  default: defaultIcon,
+  celebration: celebrationIcon,
+  cozy: cozyIcon,
+  dateNight: dateNightIcon,
+  culinary: culinaryIcon,
+};
--- src/pages/AboutPage/AboutPage.tsx
+++ src/pages/AboutPage/AboutPage.tsx
@@ -1,10 +1,12 @@
 import { AboutHero } from "@/components/AboutHero/AboutHero";
 import "./AboutPage.scss";
+import { AboutFeatures } from "@/components/AboutFeatures";
 
 export const AboutPage = () => {
   return (
     <main className="about-page">
       <AboutHero />
+      <AboutFeatures />
     </main>
   );
 };

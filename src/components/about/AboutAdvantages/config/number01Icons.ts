39ce4164ab23927a4efc7c9f97bb5afb1bca4156
nadia vlasenko <nadiia@MacBook-Air-nadia.local>
2026-06-20 23:09:44 +0200

    Add missing about advantage icons

--- src/components/about/AboutAdvantages/config/number01Icons.ts
+++ src/components/about/AboutAdvantages/config/number01Icons.ts
@@ -1,8 +1,8 @@
-import defaultIcon from "@/assets/images/aboutAdvantages/icons/01/01-default.svg";
-import celebrationIcon from "@/assets/images/aboutAdvantages/icons/01/01-celebration.svg";
-import cozyIcon from "@/assets/images/aboutAdvantages/icons/01/01-cozy-evening.svg";
-import dateNightIcon from "@/assets/images/aboutAdvantages/icons/01/01-date-night.svg";
-import culinaryIcon from "@/assets/images/aboutAdvantages/icons/01/01-culinary.svg";
+import defaultIcon from "@/assets/images/about-advantages/icons/01/01-default.svg";
+import celebrationIcon from "@/assets/images/about-advantages/icons/01/01-celebration.svg";
+import cozyIcon from "@/assets/images/about-advantages/icons/01/01-cozy-evening.svg";
+import dateNightIcon from "@/assets/images/about-advantages/icons/01/01-date-night.svg";
+import culinaryIcon from "@/assets/images/about-advantages/icons/01/01-culinary.svg";
 
 export const number01Icons = {
   default: defaultIcon,
--- src/components/about/AboutAdvantages/config/number02Icons.ts
+++ src/components/about/AboutAdvantages/config/number02Icons.ts
@@ -1,8 +1,8 @@
-import defaultIcon from "@/assets/images/aboutAdvantages/icons/02/02-default.svg";
-import celebrationIcon from "@/assets/images/aboutAdvantages/icons/02/02-celebration.svg";
-import cozyIcon from "@/assets/images/aboutAdvantages/icons/02/02-cozy-evening.svg";
-import dateNightIcon from "@/assets/images/aboutAdvantages/icons/02/02-date-night.svg";
-import culinaryIcon from "@/assets/images/aboutAdvantages/icons/02/02-culinary.svg";
+import defaultIcon from "@/assets/images/about-advantages/icons/02/02-default.svg";
+import celebrationIcon from "@/assets/images/about-advantages/icons/02/02-celebration.svg";
+import cozyIcon from "@/assets/images/about-advantages/icons/02/02-cozy-evening.svg";
+import dateNightIcon from "@/assets/images/about-advantages/icons/02/02-date-night.svg";
+import culinaryIcon from "@/assets/images/about-advantages/icons/02/02-culinary.svg";
 
 export const number02Icons = {
   default: defaultIcon,
